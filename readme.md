d268542ed6d55324bd299be2e2f717615bbe647f
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-12-12 01:20:10 +0100

    1.0.0

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Npm Search Button
+## Adds an search on npm button to every github repo with JavaScript or TypeScript.
---
+++ repo_page.js
@@ -0,0 +1,22 @@
+//BY CLIENTCRASH https://github.com/clientcrash/
+isJSRepo = false;
+reponame = reponame_e = document.querySelector("strong[itemprop=name]").children[0].innerText
+langs =  Array.from(document.querySelectorAll("span.color-fg-default.text-bold"))
+for(i=0;i<langs.length;i++){
+	e=langs[i]
+	console.log(e.innerText)
+	if(e.innerText=="JavaScript" || e.innerText=="TypeScript"){
+		isJSRepo=true
+	}
+}
+if(isJSRepo){
+	addBTN()
+	console.log("Adding search on npm Button!")
+}
+function addBTN(){
+var i_html = '<a id="npmsrchbtn" class="btn ml-2" href="https://www.npmjs.com/search?q='+reponame+'">Search <b style="color:#F7DF1E;">'+reponame+'</b> on NPM</a>'
+i_target = document.getElementsByClassName("file-navigation")[0]
+elem = document.createElement("div")
+elem.innerHTML = i_html
+i_target.appendChild(elem)
+}
